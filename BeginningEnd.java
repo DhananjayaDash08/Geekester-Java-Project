d8f8ccb15fe09621d33a00070fa99bbe97866c0c
DhananjayaDash08 <94070065+DhananjayaDash08@users.noreply.github.com>
2021-12-14 17:12:59 +0530

    Add files via upload

---
+++ BeginningEnd.java
@@ -0,0 +1,19 @@
+package Geekster_Test;
+
+public class BeginningEnd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j=99,k;
+		System.out.println("1 number from beginning and 1 number from end:");
+		for(i=1;i<=100;i++)
+		{
+			k=i+j;
+			j=j-2;
+			System.out.print(""+i+","+k+",");
+			
+		}
+
+	}
+
+}
