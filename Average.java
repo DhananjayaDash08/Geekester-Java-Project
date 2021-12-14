1f336e0bd7c7609f5809304a7f7a28dee6a3146e
DhananjayaDash08 <94070065+DhananjayaDash08@users.noreply.github.com>
2021-12-14 17:11:53 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,18 @@
+package Geekster_Test;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=12,sum=0,count=0;
+		while(i<=15)
+		{
+		   sum= sum+i;
+		   i++;
+		   count++;
+		}
+		System.out.println(sum/count);
+		
+	}
+
+}
